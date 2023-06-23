8855b00e849ad58c1b83bb763a0aaa1cb5832a17
Ashwini Singh <ashwini.kr.singh@gmail.com>
2023-06-23 10:33:14 +1000

    upgrade AzureCloudUtils class

--- commercedbsync/src/de/hybris/platform/azure/media/AzureCloudUtils.java
+++ commercedbsync/src/de/hybris/platform/azure/media/AzureCloudUtils.java
@@ -7,33 +7,76 @@
 package de.hybris.platform.azure.media;
 
 import de.hybris.platform.core.Registry;
-import de.hybris.platform.media.storage.MediaStorageConfigService.MediaFolderConfig;
+import de.hybris.platform.media.storage.MediaStorageConfigService;
 import de.hybris.platform.util.Config;
 import org.apache.commons.lang.StringUtils;
 
 public class AzureCloudUtils {
-    public AzureCloudUtils() {
+    private static final int MIN_AZURE_MEDIA_FOLDER_QUALIFIER_SIZE = 3;
+    private static final int MAX_AZURE_MEDIA_FOLDER_QUALIFIER_SIZE = 63;
+    private static final String AZURE_MEDIA_FOLDER_QUALIFIER_REGEX = "[a-z0-9-]+";
+    private static final char HYPHEN = '-';
+    private static final String DOUBLE_HYPHEN = "--";
+
+	public AzureCloudUtils() {
+	}
+
+	public static String computeContainerAddress(MediaStorageConfigService.MediaFolderConfig config) {
+		String configuredContainer = config.getParameter("containerAddress");
+		String addressSuffix = StringUtils.isNotBlank(configuredContainer)
+				? configuredContainer
+				: config.getFolderQualifier();
+		String addressPrefix = getTenantPrefix();
+		return toValidContainerName(addressPrefix + "-" + addressSuffix);
+	}
+
+	private static String toValidContainerName(String name) {
+		return name.toLowerCase().replaceAll("[/. !?]", "").replace('_', '-');
+	}
+
+	private static String toValidPrefixName(String name) {
+		return name.toLowerCase().replaceAll("[/. !?_-]", "");
+	}
+
+	private static String getTenantPrefix() {
+		// return "sys-" +
+		// Registry.getCurrentTenantNoFallback().getTenantID().toLowerCase();
+		String defaultPrefix = Registry.getCurrentTenantNoFallback().getTenantID();
+		String prefix = toValidPrefixName(Config.getString("db.tableprefix", defaultPrefix));
+		return "sys-" + prefix.toLowerCase();
+	}
+
+    public static boolean hasValidMediaFolderName(final MediaStorageConfigService.MediaFolderConfig config)
+    {
+        final String containerAddress = computeContainerAddress(config);
+        return hasValidLength(containerAddress) && hasValidFormat(containerAddress);
     }
 
-    public static String computeContainerAddress(MediaFolderConfig config) {
-        String configuredContainer = config.getParameter("containerAddress");
-        String addressSuffix = StringUtils.isNotBlank(configuredContainer) ? configuredContainer : config.getFolderQualifier();
-        String addressPrefix = getTenantPrefix();
-        return toValidContainerName(addressPrefix + "-" + addressSuffix);
+    private static boolean hasValidLength(final String folderQualifier)
+    {
+        return folderQualifier.length() >= MIN_AZURE_MEDIA_FOLDER_QUALIFIER_SIZE && folderQualifier.length()
+            <= MAX_AZURE_MEDIA_FOLDER_QUALIFIER_SIZE;
     }
 
-    private static String toValidContainerName(String name) {
-        return name.toLowerCase().replaceAll("[/. !?]", "").replace('_', '-');
+    private static boolean hasValidFormat(final String folderQualifier)
+    {
+        if (!folderQualifier.matches(AZURE_MEDIA_FOLDER_QUALIFIER_REGEX))
+        {
+            return false;
+        }
+
+        if (folderQualifier.contains(String.valueOf(HYPHEN)))
+        {
+            return hasHyphenValidFormat(folderQualifier);
+        }
+
+        return true;
     }
 
-    private static String toValidPrefixName(String name) {
-        return name.toLowerCase().replaceAll("[/. !?_-]", "");
-    }
-
-    private static String getTenantPrefix() {
-        //return "sys-" + Registry.getCurrentTenantNoFallback().getTenantID().toLowerCase();
-        String defaultPrefix = Registry.getCurrentTenantNoFallback().getTenantID();
-        String prefix = toValidPrefixName(Config.getString("db.tableprefix", defaultPrefix));
-        return "sys-" + prefix.toLowerCase();
+    private static boolean hasHyphenValidFormat(final String folderQualifier)
+    {
+        final char firstChar = folderQualifier.charAt(0);
+        final char lastChar = folderQualifier.charAt(folderQualifier.length() - 1);
+        return !folderQualifier.contains(DOUBLE_HYPHEN) && firstChar != HYPHEN && lastChar != HYPHEN;
     }
 }
